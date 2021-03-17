c2b44963ec52cc0de7bf38bf4d3e87f1a5251efb
tsb0812 <tsbauskar0812@gmail.com>
2021-03-17 13:55:23 +0530

    Class based views

--- backend/people/urls.py
+++ backend/people/urls.py
@@ -4,9 +4,14 @@ from . import views
 
 # To take advantage of the fact that our responses are no longer hardwired to a single content type let's add support for format suffixes to our API endpoints. Using format suffixes gives us URLs that explicitly refer to a given format, and means our API will be able to handle URLs such as http://example.com/api/items/4.json.
 
+# urlpatterns = [
+#     path('people/', views.people_list),
+#     path('people/<int:pk>/', views.people_detail)
+# ]
+
 urlpatterns = [
-    path('people/', views.people_list),
-    path('people/<int:pk>/', views.people_detail)
+    path('people/', views.PeopleList.as_view()),
+    path('people/<int:pk>/', views.PeopleDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/people/views.py
+++ backend/people/views.py
@@ -1,50 +1,101 @@
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
+# CLASS BASED VIEWS
+
 from .models import People
 from .serializers import PeopleSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# We will be using @csrf_exempt decorator as we want to POST to this view from client that does not have CSRF token
 
-# @api_view decorator for working with function bases views
-# APIView class for working with class based views
-
-
-@api_view(['GET', 'POST'])
-# Allow only GET and POST request for this view function
-def people_list(request, format=None):
-    if request.method == 'GET':
+class PeopleList(APIView):
+    def get(self, request, format=None):
         people = People.objects.all()
         serializer = PeopleSerializer(people, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = PeopleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-# Allow only GET, PUT and DELETE for this view function
-def people_detail(request, pk, format=None):
-    try:
-        people = People.objects.get(pk=pk)
-    except People.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class PeopleDetail(APIView):
+    def get_detail(self, pk):
+        try:
+            return People.objects.get(pk=pk)
+        except People.DoesNotExist:
+            return Http404
 
-    if request.method == 'GET':
+    def get(self, request, pk, format=None):
+        people = self.get_detail(pk)
         serializer = PeopleSerializer(people)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        people = self.get_detail(pk)
         serializer = PeopleSerializer(people, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        people = self.get_detail(pk)
         people.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# FUNCTION BASED VIEWS
+
+# from rest_framework import status
+# from rest_framework.response import Response
+# from rest_framework.decorators import api_view
+# from .models import People
+# from .serializers import PeopleSerializer
+
+# # We will be using @csrf_exempt decorator as we want to POST to this view from client that does not have CSRF token
+
+# # @api_view decorator for working with function bases views
+# # APIView class for working with class based views
+
+
+# @api_view(['GET', 'POST'])
+# # Allow only GET and POST request for this view function
+# def people_list(request, format=None):
+#     if request.method == 'GET':
+#         people = People.objects.all()
+#         serializer = PeopleSerializer(people, many=True)
+#         return Response(serializer.data)
+
+#     elif request.method == 'POST':
+#         serializer = PeopleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# # Allow only GET, PUT and DELETE for this view function
+# def people_detail(request, pk, format=None):
+#     try:
+#         people = People.objects.get(pk=pk)
+#     except People.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+
+#     if request.method == 'GET':
+#         serializer = PeopleSerializer(people)
+#         return Response(serializer.data)
+
+#     elif request.method == 'PUT':
+#         serializer = PeopleSerializer(people, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     elif request.method == 'DELETE':
+#         people.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
