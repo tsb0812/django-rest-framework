722fbcd5f1eb39920cd9f339f415fde8f9e87096
tsb0812 <tsbauskar0812@gmail.com>
2021-03-18 10:31:05 +0530

    Using mixins and generic API views

--- backend/people/views.py
+++ backend/people/views.py
@@ -1,51 +1,102 @@
-# CLASS BASED VIEWS
+# USING GENERIC CLASS-BASED VIEWS
 
 from .models import People
 from .serializers import PeopleSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 
-class PeopleList(APIView):
-    def get(self, request, format=None):
-        people = People.objects.all()
-        serializer = PeopleSerializer(people, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PeopleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PeopleList(generics.ListCreateAPIView):
+    queryset = People.objects.all()
+    serializer_class = PeopleSerializer
 
 
-class PeopleDetail(APIView):
-    def get_detail(self, pk):
-        try:
-            return People.objects.get(pk=pk)
-        except People.DoesNotExist:
-            return Http404
+class PeopleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = People.objects.all()
+    serializer_class = PeopleSerializer
 
-    def get(self, request, pk, format=None):
-        people = self.get_detail(pk)
-        serializer = PeopleSerializer(people)
-        return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        people = self.get_detail(pk)
-        serializer = PeopleSerializer(people, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# USING MIXINS
 
-    def delete(self, request, pk, format=None):
-        people = self.get_detail(pk)
-        people.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# from .models import People
+# from .serializers import PeopleSerializer
+# from rest_framework import mixins
+# from rest_framework import generics
+
+
+# class PeopleList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# class PeopleDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+#     queryset = People.objects.all()
+#     serializer_class = PeopleSerializer
+
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+
+# CLASS BASED VIEWS
+
+# from .models import People
+# from .serializers import PeopleSerializer
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
+
+
+# class PeopleList(APIView):
+#     def get(self, request, format=None):
+#         people = People.objects.all()
+#         serializer = PeopleSerializer(people, many=True)
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = PeopleSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class PeopleDetail(APIView):
+#     def get_detail(self, pk):
+#         try:
+#             return People.objects.get(pk=pk)
+#         except People.DoesNotExist:
+#             return Http404
+
+#     def get(self, request, pk, format=None):
+#         people = self.get_detail(pk)
+#         serializer = PeopleSerializer(people)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk, format=None):
+#         people = self.get_detail(pk)
+#         serializer = PeopleSerializer(people, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk, format=None):
+#         people = self.get_detail(pk)
+#         people.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 # FUNCTION BASED VIEWS
 
